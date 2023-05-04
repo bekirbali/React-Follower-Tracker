6a18a9e3366b7ba81b3d2175bde366c826c75d94
bekirbali <bfbali43@gmail.com>
2023-05-05 00:54:56 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [followings, setFollowings] = useState([]);
+  const [followers, setFollowers] = useState([]);
+
+  const getUserFollowers = async () => {
+    const { data } = await axios(
+      `https://api.github.com/users/${text}/followers`
+    );
+    setFollowers(data.map((follower) => follower.login));
+    console.log(followers);
+  };
+
+  const getUserFollowing = async () => {
+    const { data } = await axios(
+      `https://api.github.com/users/${text}/following`
+    );
+    setFollowings(data.map((following) => following.login));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getUserFollowers();
+    getUserFollowing();
+    const scumBag = () => {
+      const x = followings.filter((person) => !followers.includes(person));
+      console.log(x);
+    };
+    console.log(scumBag());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a user"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        <ul>
+          {followers.sort().map((follower, index) => (
+            <p key={follower}>
+              {index + 1} - {follower}
+            </p>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
